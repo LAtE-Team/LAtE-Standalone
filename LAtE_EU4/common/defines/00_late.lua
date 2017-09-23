7225961e1d9bf0fcb3883383d779ff29276098a9
Elzephor <charlesanthonymilton@gmail.com>
2017-09-22 23:06:09 -0400

    Initial Upload

---
+++ LAtE_EU4/common/defines/00_late.lua
@@ -0,0 +1,74 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+NDefines.NGame.START_DATE = "3053.1.1"
+NDefines.NGame.END_DATE = "3421.1.2"
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1300.0 						-- Rotation point in main menu
+NDefines.NFrontend.CAMERA_LOOKAT_Y = 0.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1519.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 1300.0 				-- Rotation point in settings
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Y = 0.0					-- Y is height
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1519.0
+NDefines.NFrontend.CAMERA_START_X = 1300.0						-- Initial position in main menu
+NDefines.NFrontend.CAMERA_START_Y = 800							-- Y is height
+NDefines.NFrontend.CAMERA_START_Z = 1400.0
+NDefines.NFrontend.CAMERA_END_X = 1300.0							-- Move to position in main menu
+NDefines.NFrontend.CAMERA_END_Y = 900.0
+NDefines.NFrontend.CAMERA_END_Z = 1400.0
+NDefines.NFrontend.CAMERA_MIN_DIST_FOR_ROTATE = 800.0 			-- Controlls when rotation starts. When camera is close enought it starts
+NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0						-- Minimum camera height
+NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3000.0						-- Maximum camera height
+NDefines.NFrontend.TIME_FROZEN = 1.0  							-- Time before initial animation starts (some deylay here so it should NOT be 0, then the animation starts before you can see it)
+NDefines.NFrontend.TIME_UNTIL_ROTATE = 1.5 						-- Time when rotation start (from begining of time)
+NDefines.NFrontend.CAMERA_SPEED_START = 0.04  					-- Initial animation speed
+NDefines.NFrontend.CAMERA_SPEED_ROTATE = 0.04						-- Rotation speed
+NDefines.NFrontend.GUI_MOVE_SPEED = 300							-- How fast sliding gui objects move ( pixels/s )
+	
+NDefines.NFrontend.FADE_IN_DONE_TIME = 2.2
+NDefines.NFrontend.GUI_START_MOVE_TIME = 1.4
+NDefines.NFrontend.CAMERA_START_MOVE_TIME = 3.0
+	
+NDefines.NFrontend.CAMERA_SPEED_IN_MENUS = 0.1
+	
+NDefines.NFrontend.FRONTEND_POS_X = 1300.0
+NDefines.NFrontend.FRONTEND_POS_Y = 900.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1500.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1300.0
+NDefines.NFrontend.FRONTEND_LOOK_Y = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1519.0
+	
+NDefines.NFrontend.SETTINGS_POS_X = 1300.0
+NDefines.NFrontend.SETTINGS_POS_Y = 551.0
+NDefines.NFrontend.SETTINGS_POS_Z = 978.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 2998.0
+NDefines.NFrontend.SETTINGS_LOOK_Y = 0.0		
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1364.0
+	
+NDefines.NFrontend.MP_OPTIONS_POS_X = 1300.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 922.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1048.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 1300.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Y = 0.0	
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1159.0
+	
+NDefines.NFrontend.TUTORIAL_POS_X = 2894.0
+NDefines.NFrontend.TUTORIAL_POS_Y = 124.0
+NDefines.NFrontend.TUTORIAL_POS_Z = 1319.0
+NDefines.NFrontend.TUTORIAL_LOOK_X = 2881.0
+NDefines.NFrontend.TUTORIAL_LOOK_Y = 21.0	
+NDefines.NFrontend.TUTORIAL_LOOK_Z = 1463.0
+	
+NDefines.NFrontend.CONTENT_POS_X = 1300.0
+NDefines.NFrontend.CONTENT_POS_Y = 922.0
+NDefines.NFrontend.CONTENT_POS_Z = 1308.0
+NDefines.NFrontend.CONTENT_LOOK_X = 1300.0
+NDefines.NFrontend.CONTENT_LOOK_Y = 0.0	
+NDefines.NFrontend.CONTENT_LOOK_Z = 1519.0
+	
+NDefines.NFrontend.CREDITS_POS_X = 3077.0
+NDefines.NFrontend.CREDITS_POS_Y = 231.0
+NDefines.NFrontend.CREDITS_POS_Z = 1508.0
+NDefines.NFrontend.CREDITS_LOOK_X = 3080.0
+NDefines.NFrontend.CREDITS_LOOK_Y = 19.0	
+NDefines.NFrontend.CREDITS_LOOK_Z = 1720.0
